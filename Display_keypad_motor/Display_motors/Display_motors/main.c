d7ab93d247cb39fc58514d662621f7d182b0a8f5
kaburia <kaburiaaustin1@gmail.com>
2023-04-01 11:31:00 +0300

    initialization

---
+++ Display_keypad_motor/Display_motors/Display_motors/main.c
@@ -0,0 +1,18 @@
+/*
+ * Display_motors.c
+ *
+ * Created: 01/04/2023 11:29:38
+ * Author : Austin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
